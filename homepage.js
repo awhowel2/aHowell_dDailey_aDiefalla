c67a4bf1aa7199b476cc93d1a9ef5537a1327a88
awhowel2 <40176325+awhowel2@users.noreply.github.com>
2020-11-18 16:55:54 -0600

    Commit

---
+++ homepage.js
@@ -0,0 +1,8 @@
+document.getElementById("demo").innerHTML = "Hello JavaScript!";
+
+var userName = document.getElementById("fname").value;
+
+document.getElementById("demo").innerHTML = userName;
+
+
+function myFunction(){}
---
+++ nodejs.js
@@ -0,0 +1,45 @@
+var http = require('http');
+//var mongodb = require('mongodb');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('homepage.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
+
+const {MongoClient} = require('mongodb');
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log( `- ${db.name}`));
+};
+
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://overlord-user:compengirules@cluster0.10bua.mongodb.net/test?retryWrites=true&w=majority";
+ 
+
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
